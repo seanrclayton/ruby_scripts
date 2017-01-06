22fbaadd71d20a4ee4b29abe85f023d6a4e91aa2
Sean Clayton <sean@seanclayton.com>
2017-01-05 19:59:34 -0500

    making script work more recursively

--- rsync_files_safe.rb
+++ rsync_files_safe.rb
@@ -4,17 +4,14 @@ require 'rsync'
 require 'shellwords'
 require 'open3'
 require 'fileutils'
+require 'find'
 
 $stdout = File.open("/root/ruby_scripts/rsync.log", "a")
 $stderr = File.open("/root/ruby_scripts/rsync_error.log","a")
 
 
 def cleanup(folder)
-	if Dir.glob("#{folder}/*").empty?
 		FileUtils.rm_rf(folder)
-	else
-		puts "there seems to still be some stuff in the folders"
-	end
 end
 
 if Dir.glob('/root/Downloads/**').empty?
@@ -22,14 +19,17 @@ if Dir.glob('/root/Downloads/**').empty?
 else 
 
 	Dir.glob('/root/Downloads/**').each do |folder|
+		files_in_dir = []
 		Dir.chdir(folder)
-    if Dir.glob("**").any?{|filename| File.extname(filename) == '.part'}
-			puts ""
-			puts "#{File.basename(folder)} still downloading..."
+  	Find.find('.') do |path| 
+      files_in_dir << File.extname(path)
+  	end
+  files_in_dir = files_in_dir.reject!{|file| file.empty?}
+		if files_in_dir.any? {|filename| filename == '.part'}
+    	puts "#{folder} still downloading"
 		elsif
-			Dir.glob("**").length == 1 && File.basename(Dir.glob("#{folder}/*")[0]) == 'rsync.lock'
+			files_in_dir.count == 1 && files_in_dir[0] == '.lock'
 			puts "Only File left is lockfile cleaning it up"	
-			File.delete("#{folder}/rsync.lock")
 			puts "Cleaning up Directory #{folder}"
 			cleanup(folder)
 		elsif Dir.glob("**").any?{|filename| File.basename(filename) == 'rsync.lock'}
@@ -41,7 +41,7 @@ else
 			puts "locking rsync"
 			File.open("#{folder}/rsync.lock", 'w') {|f| f.write(Time.now)}
 			@folder = Shellwords.escape("#{File.basename(folder)}")
-			cmd = "rsync -avr --remove-source-files --exclude 'rsync.lock' --progress  /root/Downloads/#{@folder} xxxxx@xxxxx:/home/clayton/Movies"
+			cmd = "rsync -avr --remove-source-files --exclude 'rsync.lock' --progress  /root/Downloads/#{@folder} xxxxxx@xxxxx:/home/xxx/Movies"
 			Open3.popen3(cmd) do |stdin, stdout, stderr|
 			  puts stdout.read
 			end
